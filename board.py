8af704618940e7707b60839ad286fc28cff4cf8a
Jon Brohauge <jonbrohauge@gmail.com>
2017-06-10 12:49:37 +0200

    Initial Commit

---
+++ board.py
@@ -0,0 +1,23 @@
+class Board(object):
+    """This class defines the board"""
+
+    def __init__(self, board_size):
+        """The initializer for the class"""
+        self.board_size = board_size
+        self.board = []
+
+        for index in range(0, self.board_size):
+            value = str(index)
+            self.board.append(['O'] * self.board_size)
+
+    def is_on_board(self, x_coordinate, y_coordinate):
+        """Is the piece on the board"""
+        return bool((0 <= x_coordinate < self.board_size) and (0 <= y_coordinate < self.board_size))
+
+    def updateCell(self, x_coordinate, y_coordinate, value):
+        Board[vertical][horizontal] = value
+
+    def print(self):
+        """Print the board"""
+        for row in self.board:
+            print(" ".join(row))
---
+++ sudokusolver.py
@@ -0,0 +1,14 @@
+import board
+
+int boardSize = 9
+
+board = board.Board(boardSize)
+
+def validateHorizontal():
+    return 0
+
+def validateVertical():
+    return 0
+
+def validateSquare():
+    return 0
