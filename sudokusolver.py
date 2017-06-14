51304aed93a21932d67c37c85338e554e6024b3c
Jon Brohauge <jonbrohauge@gmail.com>
2017-06-14 15:26:17 +0200

    Remove deprecated code stub

--- sudokusolver.py
+++ sudokusolver.py
@@ -3,12 +3,3 @@ import board
 BOARDSIZE = 9
 
 board = board.Board(BOARDSIZE)
-
-def validateHorizontal():
-    return 0
-
-def validateVertical():
-    return 0
-
-def validateSquare():
-    return 0
