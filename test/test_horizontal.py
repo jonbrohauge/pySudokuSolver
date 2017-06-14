691c8b632fee27e26469f617baa5f34dc585240d
Jon Brohauge <jonbrohauge@gmail.com>
2017-06-14 15:52:01 +0200

    Break solver tests into separate tests

---
+++ test/test_horizontal.py
@@ -0,0 +1,25 @@
+import unittest
+import solver
+
+
+class SolverTest(unittest.TestCase):
+
+    def setUp(self):
+        self.testSolver = solver.Solver()
+        #Horizontal test data
+
+        self.testHorizontalRowComplete = [1,2,3,4,5,6,7,8,9]
+        self.testHorizontalRowDuplicate = [1,2,3,5,5,6,7,8,9]
+        self.testHorizontalRowIncomplete = ['O',2,3,4,'O',6,7,8,9]
+        self.testHorizontalRowShort = [5,6,7,8,9]
+        self.testHorizontalRowLong = [1,2,3,4,5,6,7,8,9,10]
+
+    def test_is_horizontal_complete(self):
+        self.assertTrue(self.testSolver.is_horizontal_complete(self.testHorizontalRowComplete))
+        self.assertTrue(self.testSolver.is_horizontal_complete(self.testHorizontalRowDuplicate))
+        self.assertTrue(self.testSolver.is_horizontal_complete(self.testHorizontalRowIncomplete))
+        self.assertFalse(self.testSolver.is_horizontal_complete(self.testHorizontalRowShort))
+        self.assertFalse(self.testSolver.is_horizontal_complete(self.testHorizontalRowLong))
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ test/test_square.py
@@ -0,0 +1,27 @@
+import unittest
+import solver
+
+
+class SolverTest(unittest.TestCase):
+
+    def setUp(self):
+        self.testSolver = solver.Solver()
+
+        #Square test data
+        self.testSquareComplete = [[1,2,3],[4,5,6],[7,8,9]]
+        self.testSquareHorizontalDuplicate = [[1,2,3],[5,5,6],[7,8,9]]
+        self.testSquareVerticalDuplicate = [[1,2,4],[4,5,6],[7,8,9]]
+        self.testSquareIncomplete = [['O',2,3],[4,'O',6],[7,8,9]]
+        self.testSquareShort = [[5,6],[7,8,9]]
+        self.testSquareLong = [[1,2,3],[4,5,6],[7,8,9,10]]
+
+    def test_is_square_complete(self):
+        self.assertTrue(self.testSolver.is_square_complete(self.testSquareComplete))
+        self.assertTrue(self.testSolver.is_square_complete(self.testSquareHorizontalDuplicate))
+        self.assertTrue(self.testSolver.is_square_complete(self.testSquareVerticalDuplicate))
+        self.assertTrue(self.testSolver.is_square_complete(self.testSquareIncomplete))
+        self.assertFalse(self.testSolver.is_square_complete(self.testSquareShort))
+        self.assertFalse(self.testSolver.is_square_complete(self.testSquareLong))
+
+if __name__ == '__main__':
+    unittest.main()
